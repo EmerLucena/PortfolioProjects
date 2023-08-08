6d230314cc4e8930fbff9a6dc0e6fc44852731b1
Emerson Lucena <129839935+EmerLucena@users.noreply.github.com>
2023-08-08 13:21:53 +0800

    Update VideoGameSales Exploration.sql

--- Video Game Sales/VideoGameSales Exploration.sql
+++ Video Game Sales/VideoGameSales Exploration.sql
@@ -2,13 +2,13 @@ CREATE DATABASE VideoGameSales; -- Create a database for videogame sales and imp
 
 -- View dataset
 SELECT *
-FROM vgsalesraw;
+FROM vgsales;
 
 
 -- Counting the number of games released in every year
 SELECT 
 	Year, COUNT(Name)
-FROM vgsalesraw
+FROM vgsales
 GROUP BY Year
 ORDER BY Count(Name);
 -- 2017 = Only 3 games are recorded
@@ -19,7 +19,7 @@ ORDER BY Count(Name);
 SELECT
 	Year,
 	SUM(Global_Sales) AS Global_Sales
-FROM vgsalesraw
+FROM vgsales
 GROUP BY Year
 ORDER BY Year;
 
@@ -30,7 +30,7 @@ SELECT
 	ROUND(AVG(JP_Sales),2) AS Avg_JP_Sales,
 	ROUND(AVG(EU_Sales),2) AS Avg_EU_Sales,
 	ROUND(AVG(Other_Sales),2) AS Avg_Other_Sales
-FROM vgsalesraw
+FROM vgsales
 
 
 -- Geting the sales percentage
@@ -39,14 +39,14 @@ SELECT
 	ROUND(SUM(JP_Sales) / SUM(Global_Sales),4) AS 'JP_Sales %',
 	ROUND(SUM(EU_Sales) / SUM(Global_Sales),4) AS 'EU_Sales %',
 	ROUND(SUM(Other_Sales) / SUM(Global_Sales),4) AS 'Other_Sales %'
-FROM vgsalesraw;
+FROM vgsales;
 
 
 -- Global Sales By Genre
 SELECT
 	Genre,
 	ROUND(SUM(Global_Sales),2) AS Global_Sales
-FROM vgsalesraw
+FROM vgsales
 GROUP BY Genre
 ORDER BY Global_Sales DESC;
 
@@ -55,11 +55,11 @@ ORDER BY Global_Sales DESC;
 SELECT
 	Publisher,
 	ROUND(SUM(Global_Sales),2) AS Global_Sales
-FROM vgsalesraw
+FROM vgsales
 GROUP BY Publisher
 ORDER BY Global_Sales DESC;
 
 
 -- TOP 10 Games that generated the most sales
 SELECT TOP(10) Name, Global_Sales
-FROM vgsalesraw;
+FROM vgsales;
